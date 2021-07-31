82dd8cf2570200ba099bb79e7eb06084b7e114f7
dsbaha <>
2021-07-31 15:27:23 +0000

    fixup logging

--- main.go
+++ main.go
@@ -1,69 +1,70 @@
 package main
 
 import (
-	"io"
-	"os"
-	"fmt"
-	"net"
-	"flag"
-	"time"
-	"errors"
-	"strings"
-	"strconv"
 	"crypto/sha1"
 	"encoding/hex"
+	"errors"
+	"flag"
+	"fmt"
 	"github.com/OneOfOne/xxhash"
+	"io"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const (
-	s1ajob = "JOB,%v,%v"
-	xxhjob = "JOBXX,%v,%v"
+	s1ajob    = "JOB,%v,%v"
+	xxhjob    = "JOBXX,%v,%v"
 	minername = "dgm"
-	report = "%v,%v,%v %v,%v"
+	report    = "%v,%v,%v %v,%v"
 	SEPERATOR = ","
-	NEWLINE = "\n"
-	NULL = "\x00"
+	NEWLINE   = "\n"
+	NULL      = "\x00"
+	BUF_SIZE  = 256
 )
 
 var (
-	server = flag.String("server", os.Getenv("DUCOSERVER"), "Server Address and Port, environment variable DUCOSERVER")
-	name = flag.String("name", os.Getenv("MINERNAME"), "Miner Name, enviromnet variable MINERNAME")
-	id = flag.String("id", os.Getenv("HOSTNAME"), "Rig ID, environment variable HOSTNAME")
-	diff = flag.String("diff", os.Getenv("DIFF"), "Difficulty LOW/MEDIUM/NET, environment variable DIFF")
-	algo = flag.String("algo", os.Getenv("ALGO"), "Algorithm select xxhash/ducos1a, environment variable ALGO")
-	quiet = flag.Bool("quiet", false, "Turn off Console Logging")
-	debug = flag.Bool("debug", false, "console log send/receive messages.")
-	skip = flag.Bool("skip", false, "Skip the first 'Difficulty' Hash Range")
+	server  = flag.String("server", os.Getenv("DUCOSERVER"), "Server Address and Port, environment variable DUCOSERVER")
+	name    = flag.String("name", os.Getenv("MINERNAME"), "Miner Name, enviromnet variable MINERNAME")
+	id      = flag.String("id", os.Getenv("HOSTNAME"), "Rig ID, environment variable HOSTNAME")
+	diff    = flag.String("diff", os.Getenv("DIFF"), "Difficulty LOW/MEDIUM/NET, environment variable DIFF")
+	algo    = flag.String("algo", os.Getenv("ALGO"), "Algorithm select xxhash/ducos1a, environment variable ALGO")
+	quiet   = flag.Bool("quiet", false, "Turn off Console Logging")
+	debug   = flag.Bool("debug", false, "console log send/receive messages.")
+	skip    = flag.Bool("skip", false, "Skip the first 'Difficulty' Hash Range")
 	version = "0.1"
 )
 
 func init() {}
 
 func setDefaults() {
-	if (*name == "") {
+	if *name == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	if (*server == "") {
+	if *server == "" {
 		*server = "149.91.88.18:6000"
 	}
 
-	if (*algo == "") {
+	if *algo == "" {
 		*algo = "ducos1a"
 	}
 
-	if (*diff == "") {
+	if *diff == "" {
 		*diff = "MEDIUM"
 	}
 
-	if (*id == "") {
+	if *id == "" {
 		*id = "SETID"
 	}
 }
 
 func connect() (conn net.Conn, err error) {
-	logger("Connecting to Server: ", *server, NEWLINE)
+	logger("Connecting to Server: ", *server)
 
 	conn, err = net.Dial("tcp", *server)
 	if err != nil {
@@ -75,7 +76,7 @@ func connect() (conn net.Conn, err error) {
 		return
 	}
 
-	logger("Connected to Server Version: ", resp, NEWLINE)
+	logger("Connected to Server Version: ", resp)
 
 	return
 }
@@ -84,7 +85,7 @@ func main() {
 	flag.Parse()
 	setDefaults()
 
-	logger("Starting ds-go-miner version ", version, NEWLINE)
+	logger("Starting ds-go-miner version ", version)
 
 	conn, err := connect()
 	if err != nil {
@@ -122,11 +123,11 @@ func main() {
 
 func (j *Job) getJob(conn net.Conn) (err error) {
 	var getjobrequest string
-	switch (j.Algorithm) {
-		case "xxhash":
-			getjobrequest = fmt.Sprintf(xxhjob, *name, *diff)
-		default:
-			getjobrequest = fmt.Sprintf(s1ajob, *name, *diff)
+	switch j.Algorithm {
+	case "xxhash":
+		getjobrequest = fmt.Sprintf(xxhjob, *name, *diff)
+	default:
+		getjobrequest = fmt.Sprintf(s1ajob, *name, *diff)
 	}
 
 	err = send(conn, getjobrequest)
@@ -158,12 +159,9 @@ func (j *Job) getJob(conn net.Conn) (err error) {
 	return
 }
 
-// parses a string input, cleans it, and returns a uint64
-func parseUint(str string) (ret uint64, err error) {
-	str = strings.TrimRight(str, NULL)
-	str = strings.TrimRight(str, NEWLINE)
-	ret, err = strconv.ParseUint(str, 10, 64)
-	return
+// parses string to uint64 base 10
+func parseUint(str string) (uint64, error) {
+	return strconv.ParseUint(str, 10, 64)
 }
 
 //Reports the Job Result
@@ -189,38 +187,38 @@ func (j *Job) reportJob(conn net.Conn) (err error) {
 
 //Job is a struct for the job
 type Job struct {
-	Algorithm string
-	AlgoFunc func(*Job) error
-	NewBlock string
+	Algorithm     string
+	AlgoFunc      func(*Job) error
+	NewBlock      string
 	ExpectedBlock string
-	Result string
-	Difficulty uint64
-	Efficency float32
-	Nonce uint64
-	Sum64 uint64
+	Result        string
+	Difficulty    uint64
+	Efficency     float32
+	Nonce         uint64
+	Sum64         uint64
 }
 
 func (j *Job) ducoJob() (err error) {
 	//Set the difficulty
-	if (*skip) {
+	if *skip {
 		j.Nonce = j.Difficulty
 	}
 
-	j.Difficulty = j.Difficulty * 100 + 1
+	j.Difficulty = j.Difficulty*100 + 1
 
 	//Set the algo function
 	//var f func(*Job) error
 	switch j.Algorithm {
-		case "xxhash":
-			j.AlgoFunc = func(j *Job) (err error) {
-				return ducos1xxh(j)
-			}
-		case "ducos1a":
-			j.AlgoFunc = func(j *Job) (err error) {
-				return ducos1a(j)
-			}
-		default:
-			return errors.New("unimplemented algo")
+	case "xxhash":
+		j.AlgoFunc = func(j *Job) (err error) {
+			return ducos1xxh(j)
+		}
+	case "ducos1a":
+		j.AlgoFunc = func(j *Job) (err error) {
+			return ducos1a(j)
+		}
+	default:
+		return errors.New("unimplemented algo")
 	}
 
 	//Main job Loop
@@ -229,11 +227,11 @@ func (j *Job) ducoJob() (err error) {
 		return
 	}
 
-	if (*skip && j.Nonce >= j.Difficulty) {
+	if *skip && j.Nonce >= j.Difficulty {
 		//Getting here means searching the space prior to skip
-		j.Difficulty = (j.Difficulty-1) / 100
+		j.Difficulty = (j.Difficulty - 1) / 100
 		j.Nonce = 0
-		loggerDebug("searching skipped space ", j.Nonce, " ", j.Difficulty, NEWLINE)
+		loggerDebug("searching skipped space ", j.Nonce, " ", j.Difficulty)
 		err = j.jobLoop()
 	}
 
@@ -247,7 +245,7 @@ func (j *Job) jobLoop() (err error) {
 
 	for ; j.Nonce < j.Difficulty; j.Nonce++ {
 		err = j.AlgoFunc(j)
-		if (err != nil || j.Result == j.ExpectedBlock) {
+		if err != nil || j.Result == j.ExpectedBlock {
 			break
 		}
 	}
@@ -275,7 +273,7 @@ func ducos1xxh(j *Job) (err error) {
 	xx := xxhash.NewS64(uint64(2811))
 	nonce := strconv.FormatUint(j.Nonce, 10)
 	src := strings.NewReader(j.NewBlock + nonce)
-	
+
 	_, err = io.Copy(xx, src)
 
 	if err != nil {
@@ -290,7 +288,7 @@ func ducos1xxh(j *Job) (err error) {
 // logger is the general purpose logger
 // which can be turned off w/ cmd line switch
 func logger(msg ...interface{}) {
-	if (*quiet) {
+	if *quiet {
 		return
 	}
 
@@ -300,34 +298,46 @@ func logger(msg ...interface{}) {
 	for _, v := range msg {
 		fmt.Print(v)
 	}
+
+	fmt.Println()
 }
 
 func loggerDebug(msg ...interface{}) {
-	if (!*debug) {
+	if !*debug {
 		return
 	}
+
+	dbgmsg := []interface{}{"[DEBUG] "}
+	msg = append(dbgmsg, msg...)
 
 	logger(msg...)
 }
 
+// cleanString cleans a string
+func cleanString(str string) (ret string) {
+	ret = strings.TrimRight(str, NULL)
+	ret = strings.TrimRight(ret, NEWLINE)
+	return
+}
+
 // read is a helper for reciving a string
 func read(conn net.Conn) (ret string, err error) {
-	buf := make([]byte, 128)
-	_, err = conn.Read(buf)
+	buf := make([]byte, BUF_SIZE)
+	n, err := conn.Read(buf)
 
-	if err != nil {
+	//if error, or no bytes read
+	if err != nil || n <= 0 {
 		return
 	}
 
-	ret = string(buf)
-
-	loggerDebug("read ", ret)
+	ret = cleanString(string(buf))
+	loggerDebug("read ", n, " bytes ", ret)
 	return
 }
 
 // send is a helper for sending a string
 func send(conn net.Conn, str string) (err error) {
-	fmt.Fprintln(conn, str)
-	loggerDebug("send ", str, NEWLINE)
+	n, err := fmt.Fprintln(conn, str)
+	loggerDebug("send ", n, " bytes ", str)
 	return
 }
--- main_test.go
+++ main_test.go
@@ -7,23 +7,23 @@ import (
 var ()
 
 const (
-	lastblock = "416dc20fb261ec2dcf72147be57efc372fb765b1"
-	newblock = "dfa67daef0bbac93da38772c7bbd6e28b839bc43"
-	validateDiff = uint64(175514)
-	difficulty = uint64(7500)
-	xxLast = "f48abd686b70ffd5615fbd8c6aa8156c0425b09b"
-	xxExpected = "74c5967877c25e22"
-	xxDiff = uint64(100000)
+	lastblock      = "416dc20fb261ec2dcf72147be57efc372fb765b1"
+	newblock       = "dfa67daef0bbac93da38772c7bbd6e28b839bc43"
+	validateDiff   = uint64(175514)
+	difficulty     = uint64(7500)
+	xxLast         = "f48abd686b70ffd5615fbd8c6aa8156c0425b09b"
+	xxExpected     = "74c5967877c25e22"
+	xxDiff         = uint64(100000)
 	xxValidateDiff = uint64(4069510)
 )
 
 func TestHashingJobS1A(t *testing.T) {
 	job := &Job{
-		Algorithm: "ducos1a",
-		NewBlock: lastblock,
+		Algorithm:     "ducos1a",
+		NewBlock:      lastblock,
 		ExpectedBlock: newblock,
-		Difficulty: difficulty,
-		Nonce: validateDiff,
+		Difficulty:    difficulty,
+		Nonce:         validateDiff,
 	}
 
 	err := job.ducoJob()
@@ -39,11 +39,11 @@ func TestHashingJobS1A(t *testing.T) {
 
 func TestHashingJobXXH(t *testing.T) {
 	job := &Job{
-		Algorithm: "xxhash",
-		NewBlock: xxLast,
+		Algorithm:     "xxhash",
+		NewBlock:      xxLast,
 		ExpectedBlock: xxExpected,
-		Difficulty: xxDiff,
-		Nonce: xxValidateDiff,
+		Difficulty:    xxDiff,
+		Nonce:         xxValidateDiff,
 	}
 
 	err := job.ducoJob()
@@ -59,9 +59,9 @@ func TestHashingJobXXH(t *testing.T) {
 
 func BenchmarkDUCOS1A(b *testing.B) {
 	job := &Job{
-		NewBlock: lastblock,
+		NewBlock:      lastblock,
 		ExpectedBlock: newblock,
-		Difficulty: difficulty,
+		Difficulty:    difficulty,
 	}
 
 	var err error
@@ -76,9 +76,9 @@ func BenchmarkDUCOS1A(b *testing.B) {
 
 func BenchmarkDUCOS1A2(b *testing.B) {
 	job := &Job{
-		NewBlock: lastblock,
+		NewBlock:      lastblock,
 		ExpectedBlock: newblock,
-		Difficulty: difficulty,
+		Difficulty:    difficulty,
 	}
 
 	var err error
@@ -93,13 +93,13 @@ func BenchmarkDUCOS1A2(b *testing.B) {
 
 func BenchmarkXXHash(b *testing.B) {
 	job := &Job{
-		NewBlock: xxLast,
+		NewBlock:      xxLast,
 		ExpectedBlock: xxExpected,
-		Difficulty: xxDiff,
+		Difficulty:    xxDiff,
 	}
 
 	var err error
-	for i := 0 ; i < b.N ; i++ {
+	for i := 0; i < b.N; i++ {
 		job.Nonce += uint64(i)
 		err = ducos1xxh(job)
 		if err != nil {
